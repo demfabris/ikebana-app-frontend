b4db80a2ef20cbeafc62bccd92d50344669df831
fabricio7p <fabriciodematte7p@gmail.com>
2020-05-27 14:16:34 -0300

    starting feature dev

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://api.fabricio7p.com.br'
+    // baseURL: 'https://api.fabricio7p.com.br'
+    //dev
+    baseURL: 'http://localhost:5000'
 });
 
 export default api;
